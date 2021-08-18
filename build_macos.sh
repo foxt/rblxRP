e1ace529068952bf0323eb6441803b174d2d0ae2
Taylor Robinson <tayyiorrobinson@gmail.com>
2021-08-18 02:26:26 +0100

    move things to make packager happy

--- build_macos.sh
+++ build_macos.sh
@@ -1,6 +1,6 @@
 echo "[macOS] Building"
 
-npx @nodegui/packer --pack ./build/      # Pack the app
+npx @nodegui/packer --pack ./app/      # Pack the app
 
 echo "[macOS] Branding"
 cp -R ico ./deploy/darwin/build/rblxRP.app/Contents/Resources/
